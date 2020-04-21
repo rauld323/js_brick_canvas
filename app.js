ceba90fe894daf7bcd19f1f9e0e8abb9d2e7946f
Raul Davila <rauldavila323@gmail.com>
2020-04-21 10:37:19 -0500

    movement has been giving to the ball

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width / 2; //x and y will determine where to star on the cancas
+var y = canvas.height - 30;
+var dx = 1; // controls speed
+var dy = -1;
+var ballRadius = 10;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clearReact() cleans up the traces of the ball
+  drawBall();
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+  //Determines when the ball should return
+  // we use "-ballRadius" because we want to stop the ball the moment it touches the canvas wall
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  x += dx; // Control Speed
+  y += dy;
+}
+//You can run function over and over using
+//"setInterval"
+setInterval(draw, 10);
