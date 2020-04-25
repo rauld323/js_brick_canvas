6d5130ead83183506975cb5bcd30fc1abbf2f259
Raul Davila <rauldavila323@gmail.com>
2020-04-25 16:59:52 -0500

    Paddle was added

--- app.js
+++ app.js
@@ -6,6 +6,36 @@ var dx = 1; // controls speed
 var dy = -1;
 var ballRadius = 10;
 
+//paddly begins here.
+var paddleHeight = 10; // height  
+var paddleWidth = 75; // width
+var paddleX = (canvas.width-paddleWidth)/2; //starting point on axis
+
+//press buttons
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -13,19 +43,43 @@ function drawBall() {
   ctx.fill();
   ctx.closePath();
 }
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); //clearReact() cleans up the traces of the ball
   drawBall();
+  drawPaddle();
 
   //Determines when the ball should return
   // we use "-ballRadius" because we want to stop the ball the moment it touches the canvas wall
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval);
+    }
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  }
+  else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
 
   x += dx; // Control Speed
@@ -33,4 +87,6 @@ function draw() {
 }
 //You can run function over and over using
 //"setInterval"
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
+
+
